e550917a339a246b9c46f6aa7a9f69fad3c14539
Adrian Daniel Morales <104307198+Adrianmorales1@users.noreply.github.com>
2022-05-18 19:10:17 -0500

    Add files via upload

---
+++ likes/script.js
@@ -0,0 +1,23 @@
+var nielh4= document.querySelector("#nielLike");
+var nicoleh4 = document.querySelector("#nicoleLike");
+var jimh4 = document.querySelector("#jimLike");
+console.log(nielh4);
+console.log(nicoleh4);
+console.log(jimh4);
+var count;
+
+function NMlike() {
+    
+    nielh4.innerText = "10 like(s)";
+    console.log("liked Niels!");
+}
+
+function NKlike() {
+    nicoleh4.innerText = "13 like(s)";
+    console.log("liked Nicoles!");
+}
+
+function JRlike() {
+    jimh4.innerText = "10 like(s)";
+    console.log("liked Jims!");
+}
